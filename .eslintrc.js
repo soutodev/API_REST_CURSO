2d0895cb49d7d8e6444c99ca40bb1d566d516c1d
Kevin Souto <kevinsoutods@gmail.com>
2022-06-24 01:52:02 -0300

    Criando usuários

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': 0,
     'class-methods-use-this': 'off',
     'import/first': 'off',
+    'no-param-reassign': 'off',
   },
 };
---
+++ src/database/migrations/20220624035401-users.js
@@ -0,0 +1,44 @@
+// criação dos models na base de dados
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  },
+};
