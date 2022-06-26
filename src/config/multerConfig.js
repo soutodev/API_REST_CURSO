986d4cf529beb9957b1b0a57ce22ab413302158c
Kevin Souto <kevinsoutods@gmail.com>
2022-06-25 22:39:00 -0300

    Tratando os arquivos enviados

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,6 +4,16 @@ import { extname, resolve } from 'path';
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  // filtrando e tratando o tipo de arquivo a ser enviado
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPEG'));
+    }
+
+    return cb(null, true);
+  },
+
+  // direcionando e nomeando os arquivos a ser feito o upload
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
