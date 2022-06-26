5c62516ef55ad839d4d10b8aff0f603eb88c3adb
Kevin Souto <kevinsoutods@gmail.com>
2022-06-26 01:11:00 -0300

    Tratando os arquivos estáticos e destinando para 'uploads/images/..'

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -18,7 +18,7 @@ export default {
   // direcionando e nomeando os arquivos a ser feito o upload
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, resolve(__dirname, '..', '..', 'uploads'));
+      cb(null, resolve(__dirname, '..', '..', 'uploads', 'images'));
     },
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -8,7 +8,7 @@ class AlunoController {
       order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
       include: {
         model: Foto,
-        attributes: ['filename'],
+        attributes: ['url', 'filename'],
       },
     });
     res.json(alunos);
@@ -69,7 +69,7 @@ class AlunoController {
         order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
         include: {
           model: Foto,
-          attributes: ['filename'],
+          attributes: ['url', 'filename'],
         },
       });
 
